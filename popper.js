eb0ac9204c1be0884871b132b2e32d07dd7b16fc
JM Lontoc <jm.lontoc@thinkconveyancing.com.au>
2019-10-16 09:05:29 +0800

    separated popper and loader

--- popper.js
+++ popper.js
@@ -3,36 +3,107 @@
 class PopperJM {
 
     static getSelector() { return "popper-jm" }
+    static getLoaderSelector() { return "loader-jm" }
 
+    /**
+     * 
+     * @param {*} args 
+     */
     static createPopper(args) {
+        if (!PopperJM.hasPopper()) {
+            PopperJM.generatePopper(args)
+        } else {
+            document.getElementById(PopperJM.getSelector()).remove()
+            PopperJM.generatePopper(args)
+        }
+    }
 
+    static createLoader(args) {
+        if (!PopperJM.hasLoader()) {
+            PopperJM.generateLoader(args)
+        } else {
+            document.getElementById(PopperJM.getSelector()).remove()
+            PopperJM.generateLoader(args)
+        }
+    }
+
+    static generatePopper(args) {
         let { color } = args
         let { message } = args
         let { body } = args
         let { duration } = args
         let { icon } = args
+        let { position } = args
 
-        let popper = new Popper(color, message, icon)
+        let popper = new Popper(color, message, icon, position, 0)
         body.appendChild(popper.getDisplay())
 
         setTimeout(() => { PopperJM.hide() }, duration * 1000)
     }
 
+    static generateLoader(args) {
+        let { color } = args
+        let { message } = args
+        let { body } = args
+        let { position } = args
+
+        let icon = "fas fa-spinner fa-spin"
+        let popper = new Popper(color, message, icon, position, 1)
+        body.appendChild(popper.getDisplay())
+    }
+
+    static removePopper() {
+        let popper = document.getElementById(PopperJM.getSelector())
+        if (popper) {
+            popper.remove()
+        }
+    }
+
+    static removeLoader() {
+        let loader = document.getElementById(PopperJM.getLoaderSelector())
+        if (loader) {
+            loader.remove()
+        }
+    }
+
     static hide() {
         document.getElementById(PopperJM.getSelector()).style.display = 'none'
+    }
+
+    static hasPopper() {
+        let selector = PopperJM.getSelector()
+        let popper = document.getElementById(selector)
+        return popper != null
+    }
+
+    static hasLoader() {
+        let selector = PopperJM.getLoaderSelector()
+        let loader = document.getElementById(selector)
+        return loader != null
     }
 }
 
 class Popper {
 
-    constructor(color, message, faIcon) {
+    /**
+     * 
+     * @param {string} color    Background color of the feedback box
+     * @param {string} message  Message inside the feedback box
+     * @param {string} faIcon   Font Awesome Icon "in string" to be displayed inside the box
+     * @param {number} position Refer to PopperJM position variables for values
+     * @param {number} isLoader	If popper is a loader
+     */
+    constructor(color, message, faIcon, position, isLoader) {
         this.color = color
         this.message = message
         this.faIcon = faIcon
+        this.position = position
+	this.isLoader = isLoader
 
         this.getDisplay = () => {
             let html = document.createElement("div")
-            html.setAttribute("id", PopperJM.getSelector())
+
+	    !this.isLoader ? html.setAttribute("id", PopperJM.getSelector()): html.setAttribute("id", PopperJM.getLoaderSelector())
 
             let messageHolder = document.createElement("span")
             let messageNode = document.createTextNode(this.message)
@@ -55,15 +126,35 @@ class Popper {
             let styles = {
                 background: this.color,
                 position: 'fixed',
-                bottom: 0,
-                right: 0,
                 margin: '2rem',
                 padding: '1rem',
                 color: 'white',
                 'font-family': 'Sans-serif',
                 'border-radius': '10px',
-                'z-index': 99,
+                'z-index': 9999,
                 'opacity': 0.8
+            }
+
+            switch (this.position) {
+                case PopperJM.BOTTOM_RIGHT:
+                    styles.bottom = 0
+                    styles.right = 0
+                    break
+
+                case PopperJM.BOTTOM_LEFT:
+                    styles.bottom = 0
+                    styles.left = 0
+                    break
+
+                case PopperJM.TOP_LEFT:
+                    styles.top = 0
+                    styles.left = 0
+                    break
+
+                case PopperJM.TOP_RIGHT:
+                    styles.top = 0
+                    styles.right = 0
+                    break
             }
 
             let elementStyle = html.style
@@ -73,5 +164,13 @@ class Popper {
         }
     }
 }
+
+/**
+ * Popper position variables
+ */
+PopperJM['TOP_LEFT'] = 1
+PopperJM['TOP_RIGHT'] = 2
+PopperJM['BOTTOM_LEFT'] = 3
+PopperJM['BOTTOM_RIGHT'] = 0
 
 module.exports = PopperJM
